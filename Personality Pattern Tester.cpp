82d84b9cd6e645f5047b27d2a1bc3d7dd4d3865c
Aidan Litschi <AidLit766@bctcstudent.org>
2026-01-06 13:58:25 -0500

    Added answer key array

--- Personality Pattern Tester.cpp
+++ Personality Pattern Tester.cpp
@@ -2,19 +2,70 @@
 //
 
 #include <iostream>
+using namespace std;
+
+char answerKey[51][4] =
+{
+    // Double point questions
+    {'B', 'D', 'C', 'A'},// 1
+    {'B', 'D', 'A', 'C'},// 2
+    {'A', 'D', 'C', 'B'},// 3
+    {'D', 'B', 'A', 'C'},// 4
+    {'C', 'A', 'D', 'B'},// 5
+    {'B', 'A', 'D', 'C'},// 6
+    {'D', 'A', 'C', 'B'},// 7
+    {'D', 'B', 'C', 'A'},// 8
+    {'D', 'B', 'A', 'C'},// 9
+    {'A', 'B', 'D', 'C'},// 10
+    {'D', 'A', 'B', 'C'},// 11
+    {'D', 'A', 'C', 'B'},// 12
+    {'C', 'A', 'B', 'D'},// 13
+    // Single point questions
+    {'B', 'B', 'A', 'A'},// 14
+    {'A', 'B', 'B', 'A'},// 15
+    {'A', 'A', 'B', 'B'},// 16
+    {'A', 'B', 'B', 'A'},// 17
+    {'A', 'A', 'B', 'B'},// 18
+    {'B', 'B', 'A', 'A'},// 19
+    {'A', 'B', 'B', 'A'},// 20
+    {'B', 'A', 'A', 'B'},// 21
+    {'A', 'B', 'B', 'A'},// 22
+    {'B', 'B', 'A', 'A'},// 23
+    {'A', 'A', 'B', 'B'},// 24
+    {'A', 'B', 'B', 'A'},// 25
+    {'B', 'A', 'A', 'B'},// 26
+    {'B', 'B', 'A', 'A'},// 27
+    {'B', 'B', 'A', 'A'},// 28
+    {'A', 'A', 'B', 'B'},// 29
+    {'B', 'A', 'A', 'B'},// 30
+    {'A', 'B', 'B', 'A'},// 31
+    {'B', 'A', 'A', 'B'},// 32
+    {'B', 'B', 'A', 'A'},// 33
+    {'B', 'B', 'A', 'A'},// 34
+    {'A', 'B', 'B', 'A'},// 35
+    {'A', 'A', 'B', 'B'},// 36
+    {'A', 'B', 'B', 'A'},// 37
+    {'B', 'B', 'A', 'A'},// 38
+    {'A', 'B', 'B', 'A'},// 39
+    {'B', 'B', 'A', 'A'},// 40
+    {'A', 'B', 'B', 'A'},// 41
+    {'B', 'B', 'A', 'A'},// 42
+    {'A', 'B', 'B', 'A'},// 43
+    {'A', 'A', 'B', 'B'},// 44
+    {'B', 'A', 'A', 'B'},// 45
+    {'A', 'A', 'B', 'B'},// 46
+    {'A', 'B', 'B', 'A'},// 47
+    {'B', 'B', 'A', 'A'},// 48
+    {'B', 'A', 'A', 'B'},// 49
+    {'A', 'A', 'B', 'B'},// 50
+    {'A', 'B', 'B', 'A'},// 51
+};
+
+
+
 
 int main()
 {
+
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
