85b6a5832818b7dde5fc83f3b60436a728284eca
Jensen Dries <JenDri764@bctcstudent.org>
2026-01-07 13:04:02 -0500

    - Added Anwser keys to both double and single question headers.
    - Added Questions and anwsers to the double header.

--- dblPointQuestions.h
+++ dblPointQuestions.h
@@ -5,3 +5,22 @@ using namespace std;
 
 #pragma endregion
 
+using namespace std;
+
+char answerKey[13][4] =
+{
+    // Double point questions
+    {'B', 'D', 'C', 'A'},// 1
+    {'B', 'D', 'A', 'C'},// 2
+    {'A', 'D', 'C', 'B'},// 3
+    {'D', 'B', 'A', 'C'},// 4
+    {'C', 'A', 'D', 'B'},// 5
+    {'B', 'A', 'D', 'C'},// 6
+    {'D', 'A', 'C', 'B'},// 7
+    {'D', 'B', 'C', 'A'},// 8
+    {'D', 'B', 'A', 'C'},// 9
+    {'A', 'B', 'D', 'C'},// 10
+    {'D', 'A', 'B', 'C'},// 11
+    {'D', 'A', 'C', 'B'},// 12
+    {'C', 'A', 'B', 'D'},// 13
+};
