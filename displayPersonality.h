f58c2cef299c95f1943ad0894a285c8f52351d5f
Jensen Dries <JenDri764@bctcstudent.org>
2026-01-08 13:39:55 -0500

    Added personality display for Cooperator

--- displayPersonality.h
+++ displayPersonality.h
@@ -1,3 +1,36 @@
+// Displays personality
+// Written by Jensen Dries (typing all of this was not enjoyable)
 #include <string>
 using namespace std;
 
+void CooperatorDisplay()
+{
+	cout << "Your personality type is: Cooperator \n \n";
+
+	cout << "Your traits include: \n- Being concerned about feelings \n- Caring less about facts \n- Your flexable \n- You perfer low- risk situations \n- You avoid threatening situations \n- And your cooperative and people-oriented \n \n";
+
+	cout << "The type of question you ask is, \"How?\" and your statement is \"Tell me how you feel\" \n";
+	cout << "Your fears include loss of security and change \n \n";
+
+	cout << "Your strengths include: \n- Commitment \n- Steadiness \n- Caring \n- Loyalty \n- Cooperation \n \n";
+
+	cout << "You respond well to: \n- Sincerity \n- Loyalty \n- Assurance \n- Cooperation\n \n";
+
+	cout << "Your barriers are: \n- Lack of direction \n- Uncertainty \n- Slow to action \n \n";
+	cout << "Your need to develop for effectiveness: \n- Emotional Control \n- Action-oriented \n- Tendency \n- Risk taking ability \n- Objectivity \n- Critical thinking \n \n";
+}
+
+void AnalyzerDisplay()
+{
+
+}
+
+void RegulatorDisplay()
+{
+
+}
+
+void EnergizerDisplay()
+{
+	
+}
